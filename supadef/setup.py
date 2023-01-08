f27a1ca74cfb8b32ea82e511879257a52d9d0514
connormcl <connormcl14@gmail.com>
2023-01-07 19:19:47 -0500

    first commit

---
+++ supadef/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='supadef',
+    version='0.1.0',
+    description='client for supadef platform',
+    url='TODO',
+    author='Connor McLaughlin',
+    author_email='connor@fractall.com',
+    packages=['supadef'],
+    install_requires=[],
+
+    classifiers=[
+        'Programming Language :: Python :: 3.10',
+    ],
+)
