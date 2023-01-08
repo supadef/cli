3c1a9dddec8663f17103cf30069492b1d6081eb0
connormcl <connormcl14@gmail.com>
2023-01-07 22:00:55 -0500

    hello world skeleton

---
+++ supadef/cli.py
@@ -0,0 +1,14 @@
+from typer import Typer
+
+app = Typer()
+
+@app.command()
+def hello():
+    print("Hello.")
+
+@app.command()
+def bye(name: str):
+    print(f"Bye {name}")
+
+if __name__ == "__main__":
+    app()
