206b63cc338ccfcf1b49586b364e5da4e3cf0ee9
connormcl <connormcl14@gmail.com>
2023-01-15 01:37:24 -0500

    supadef init skeleton

--- supadef/__init__.py
+++ supadef/__init__.py
@@ -1,1 +1,16 @@
 VERSION = "0.0.3"
+
+
+def endpoint(route: str):
+    """define a function as a web endpoint"""
+    pass
+
+
+def image(matrix):
+    """from a matrix of data, show an image"""
+    pass
+
+
+def mapviewer():
+    pass
+
--- supadef/cli.py
+++ supadef/cli.py
@@ -1,14 +1,40 @@
+import subprocess
 from typer import Typer
 
+
+def execute_bash_command(cmd):
+    print(cmd)
+    process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    if process.returncode != 0:
+        raise subprocess.CalledProcessError(process.returncode, cmd, output=stdout, stderr=stderr)
+    return stdout.decode().strip()
+
+
 app = Typer()
+
 
 @app.command()
 def hello():
     print("Hello.")
 
+
 @app.command()
 def bye(name: str):
     print(f"Bye {name}")
+
+
+@app.command()
+def init(project_name: str):
+    # 1.
+    execute_bash_command(f'mkdir -p {project_name}')
+
+
+@app.command()
+def push():
+    print('push')
+    pass
+
 
 if __name__ == "__main__":
     app()
