03b6bb12ad3eac5953cc91904c5f3d01da7dc7ce
supabot <supabot@supadef.com>
2024-03-09 17:05:07 -0800

    update links

--- supadef/links.py
+++ supadef/links.py
@@ -9,11 +9,19 @@ def route(slug: str, loader=False) -> str:
         'user': '/{username}',
         'user > billing': '/{username}/billing',
         'user > account': '/{username}/account',
+        'user > project': '/{username}/{project_name}',
         'user > project > functions > run': '/fn/run/{run_id}',
         'user > project > danger zone': '/{username}/{project}/danger_zone',
         'user > project > env': '/{username}/{project}/env',
         'docs > cli > supadef connect': '',
-        'supadef connect': ''
+        'supadef connect': '/email',
+        'supadef projects': '/projects',
+        'supadef create': '/project',
+        'supadef logs': '/fn/logs/{run_id}',
+        'supadef push': '/project/{name}/upload_package',
+        'supadef run': '/run',
+        'supadef destroy': '/project',
+        'supadef set_env': '/project/{name}/set_env'
     }
     if slug not in slug_to_pattern.keys():
         raise ValueError(f'no such slug: {slug}')
@@ -29,10 +37,12 @@ def link(slug: str,
          loader=False,
          username: str = None,
          project_name: str = None,
+         run_id: str = None,
          service_endpoint: str = SERVICE_ENDPOINT) -> str:
     subs = {
         'username': username,
-        'project_name': project_name
+        'project_name': project_name,
+        'run_id': run_id
     }
     pattern = route(slug, loader).format(**subs)
 
