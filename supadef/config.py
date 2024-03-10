4e4158a49d6b4fa9ebd0f8672e18116f845158a9
supabot <supabot@supadef.com>
2024-03-09 17:32:37 -0800

    update service endpoint

--- supadef/config.py
+++ supadef/config.py
@@ -1,4 +1,4 @@
 TIMEOUT_SECONDS = 3 * 60
-# SERVICE_ENDPOINT = 'https://supadef.com'
-SERVICE_ENDPOINT = 'http://localhost:8000'
+SERVICE_ENDPOINT = 'https://supadef.com'
+# SERVICE_ENDPOINT = 'http://localhost:8000'
 LOCAL_CREDS_PATH = '~/.supadef/credentials.yml'
